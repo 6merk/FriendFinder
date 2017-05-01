361c972f76eb39b66eb5b4c2054a3d4b8a3e18ee
Mark Kahler <kahler6@gmail.com>
2017-04-30 21:50:29 -0700

    Add files via upload

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,7 +1,10 @@
-========================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var friends = require("../data/friends.js");
+
 
 
 // ===============================================================================
@@ -9,12 +12,17 @@ var waitListData = require("../data/waitinglistData");
 // ===============================================================================
 
 module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
 
-  
-  
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
 
- app.post("/api/clear", function() {
+ app.post("/api/friends", function() {
+
+ }
