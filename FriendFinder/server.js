a08103074d437f930c63409800b0634f54d48ed9
Mark Kahler <kahler6@gmail.com>
2017-05-02 16:02:58 -0700

    Update server.js

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -5,11 +5,10 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
 
-// Sets up the Express app to handle data parsing
+//Data parsing from express
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
